a7c4e9a4eef43fc0c97b3edad77c1f5ab9cf0c4f
Muhammad Rizal Afrizal <myheadoverloadiii@gmail.com>
2025-02-19 20:03:18 +0700

    menghilangkan debugbar

--- resources/js/Components/PostList.vue
+++ resources/js/Components/PostList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full bg-white shadow-md border rounded-lg">
-        <h2 class="font-semibold text-lg p-6 bg-blue-700 text-white rounded-lg">
+        <h2 class="font-semibold text-lg p-6 bg-blue-600 text-white rounded-lg">
             <slot name="title" />
         </h2>
         <ul
